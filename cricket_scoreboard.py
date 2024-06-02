206a89a5976a825613edfa218f6bc64b790b467a
Pradip Dharam <pradip.dharam@gmail.com>
2024-06-02 16:37:25 +0530

    As cpu cycles were getting wasted because of pull model. Needed to use push model, publisher CricketScoreBoard calls the update method of 3 subscribers. In 3 subscribers, no more while loop is required. Still this pub-sub model has design flaws; Dependency inversion & Open closed principles in SOLID are violated. Further, interface & concretions to be introduced or something which abides SOLID principles.

--- cricket_scoreboard.py
+++ cricket_scoreboard.py
@@ -1,12 +1,23 @@
+from projected_score_board import ProjectedScoreBoard
+from run_rate_board import RunRateBoard
+from top_discussion_board import TopDiscussionBoard
+
+
 class CricketScoreBoard:
     __runs: int
     __wickets: int
     __over: float
+    __projected_score_board: ProjectedScoreBoard
+    __run_rate_board: RunRateBoard
+    __top_discussion_board: TopDiscussionBoard
 
     def update_score(self, runs: int, wickets: int, over: float):
         self.__runs = runs
         self.__wickets = wickets
         self.__over = over
+        self.__projected_score_board.update(runs, wickets, over)
+        self.__run_rate_board.update(runs, wickets, over)
+        self.__top_discussion_board.update(runs, wickets, over)
 
     @property
     def runs(self):
--- top_discussion_board.py
+++ top_discussion_board.py
@@ -15,27 +15,24 @@ class TopDiscussionBoard:
         self.__cricket_score_board = cricket_score_board
 
     def update(self, runs: int, wickets: int, over: float):
-        while True:
-            is_updated = False
-            updated_runs = self.__cricket_score_board.runs
-            if updated_runs != self.__runs:
-                self.__runs = updated_runs
-                is_updated = True
+        is_updated = False
+        updated_runs = self.__cricket_score_board.runs
+        if updated_runs != self.__runs:
+            self.__runs = updated_runs
+            is_updated = True
 
-            updated_wickets = self.__cricket_score_board.wickets
-            if updated_wickets != self.__wickets:
-                self.__wickets = updated_wickets
-                is_updated = True
+        updated_wickets = self.__cricket_score_board.wickets
+        if updated_wickets != self.__wickets:
+            self.__wickets = updated_wickets
+            is_updated = True
 
-            updated_overs = self.__cricket_score_board.over
-            if updated_overs != self.__over:
-                self.__over = updated_overs
-                is_updated = True
+        updated_overs = self.__cricket_score_board.over
+        if updated_overs != self.__over:
+            self.__over = updated_overs
+            is_updated = True
 
-            if is_updated:
-                # persists details in db
-                # calculate projected score
-                # update the board display with projected score
-                pass
-
-
+        if is_updated:
+            # persists details in db
+            # calculate projected score
+            # update the board display with projected score
+            pass
